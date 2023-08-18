a212831f4cb0db333a56a759fa892405229e6449
har2525life <har2525life@outlook.com>
2023-08-18 23:43:16 +0900

    fix:test code

--- tests/exsample.spec.ts
+++ tests/exsample.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test.describe("Login 成功", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("http://localhost:3000");
-    await page.getByTestId("email").fill("sample4@gmail.com");
+    await page.getByTestId("email").fill("sample10@gmail.com");
     await page.getByTestId("password").fill("testsample");
 
     await page.getByTestId("signIn").click();
@@ -26,27 +26,4 @@ test.describe("Login 成功", () => {
 });
 
 
-test.describe("Login 失敗", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto("http://localhost:3000");
-    await page.getByTestId("email").fill("sample2@gmail.com");
-    await page.getByTestId("password").fill("testsample");
 
-    await page.getByTestId("signIn").click();
-
-    await page.screenshot({
-      fullPage: true,
-      path: "./tests/exsample_test/email_input2.png",
-    });
-  });
-
-  test("Login 成功 /about ページ表示されない", async ({ page }) => {
-    const result = page.getByTestId("mainPage");
-    await expect(result).toHaveText("Sign in");
-
-    await page.screenshot({
-        fullPage: true,
-        path: "./tests/exsample_test/login_success2.png",
-      });
-  });
-});
